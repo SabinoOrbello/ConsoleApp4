02d410e0718b109cbdaad23966de13d373784313
SabinoOrbello <gemellus95@outlook.it>
2024-01-30 15:47:51 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp4/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Età {  get; set; }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        public int GetEta()
+        {
+            return Età;
+        }
+
+        public string GetDettagli()
+        {
+            return $"Nome : {Nome}, Cognome: {Cognome}, Età: {Età}";
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+
+            persona1.Nome = "MArio";
+            persona1.Cognome = "Rossi";
+            persona1.Età = 30;
+
+            Console.WriteLine($"Nome : {persona1.GetNome()}");
+            Console.WriteLine($"Cognome : {persona1.GetCognome()}");
+            Console.WriteLine($"Età : {persona1.GetEta()}");
+            Console.WriteLine($"Dettagli : {persona1.GetDettagli()}");
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
